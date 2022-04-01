c3139010571c8e197e4d57c88dd762bbace41af4
Colehubert <Colehubert2001@gmail.com>
2022-04-01 15:20:15 -0700

    making the questions for quiz

---
+++ Assets/script.js
@@ -0,0 +1,23 @@
+var questionArray = [
+{
+    question: "who won superbowl 48?",
+    choices: ["Seahawks", "Broncos", "patriots", "49rs"],
+    answer:"Seahawks",
+},
+{
+    question: "who won the nba finals in 2021?",
+    choices: ["Bucks","Suns","Nuggets","Hawks"],
+    answer: "Bucks",
+},
+{
+
+    question: "who won the 2021 world series?",
+    choices: ["Braves","Dodgers","Yankees","Astros"],
+    answer: "Braves"
+},
+{
+    question: "who won the mls cup in 2021?",
+    Choices: ["New York FC","Portland Timbers","Seattle Sounders","LA Galaxy"],
+    answer: "New York FC"
+},
+];
